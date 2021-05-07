f4e3719f20a6eaa887ed3d8b7a581a83d81453a0
Bouzas1402 <72886309+Bouzas1402@users.noreply.github.com>
2021-05-07 17:10:23 +0200

    Add files via upload

---
+++ conexionBBDD/App-Practica/src/App.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class App {
+public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//Crear la base de datos App.db si no existe. Si existe, conectar con ella cada vez que se
+//ejecute el programa. La base de datos contiene una única tabla Usuarios, con los
+//campos Id (numérico, clave primaria; Username, Email, Password y Estado (posibles
+//valores: ACTIVO, PENDIENTE, BLOQUEADO), todos ellos de texto y máximo 100
+//caracteres.
+
+
+    }
+    //void añadirRegistro(Connection con): pide por teclado los datos
+    //necesarios para rellenar un registro de App.db, y los inserta en
+    //la base de datos. Comprueba su funcionamiento añadiendo tres
+    //usuarios desde el método main. Una vez hayas implementado, más
+    //adelante, el método obtenerÚltimoId(Statement st), implementa su
+    //utilización en este método para generar el Id de usuario
+    //automáticamente. Utilizar PreparedStatement
+}
+
