65e65cf52644bc8c251a1cdfbab924de209fcd25
Bouzas1402 <72886309+Bouzas1402@users.noreply.github.com>
2021-05-11 20:27:14 +0200

    Add files via upload

---
+++ conexionBBDD/src/com/company/conexion.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import org.sqlite.SQLiteDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class conexion {
+    public static void main(String[] args) {
+        String url = "jdbc:sqlite:D:\\Educa\\daw1_a\\sqlite\\chinook.db";
+        SQLiteDataSource dataSource = new SQLiteDataSource();
+        dataSource.setUrl(url);
+
+        try (Connection con = dataSource.getConnection()) {
+            if (con.isValid(5)) {
+                System.out.printf("Conexión establecida");
+            }
+            //
+            try (Statement st = con.createStatement()){
+                String sql = "CREATE TABLE quevedofest (id INT CONSTRAINT quevedofest_pk PRIMARY KEY, nombre VARCHAR(15), ubicacion VARCHAR(10), capacidad INT, web VARCHAR(255)";
+
+
+            }
+
+
+            //
+        } catch (SQLException throwables) {
+            System.out.println("Excepción al establecer la conexión " + throwables.getSQLState());
+        }
+    }
+}
